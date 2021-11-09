9a6cae90a82d80ce3a6bdb9742d56c6703365a80
MdWahiduzzamanEmon <wahedemon09@gmail.com>
2021-11-10 01:16:49 +0600

    heroku setup

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-
+const { MongoClient } = require("mongodb");
 
 
 const port = process.env.PORT ||5000;
@@ -13,6 +13,15 @@ app.use(cors())
 app.use(express.json())
 
 
+// database connect 
+
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zpne0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+console.log("Connected database", uri);
 app.get("/", (req, res) => {
   res.send("Hello Watch World!");
 });
