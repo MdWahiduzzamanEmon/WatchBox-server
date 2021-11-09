b0ab5384cb05c678c2992207d2e893ca3b0c7727
MdWahiduzzamanEmon <wahedemon09@gmail.com>
2021-11-10 01:05:18 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+
+
+
+const port = process.env.PORT ||5000;
+
+
+//middleware 
+app.use(cors())
+app.use(express.json())
+
+
+app.get("/", (req, res) => {
+  res.send("Hello Watch World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
